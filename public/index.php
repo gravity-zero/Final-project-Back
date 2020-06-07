47bba574a3674331cc0b090d0d989fa3064aed0e
gravity-zero <gravity.neo@gmail.com>
2020-06-07 20:34:05 +0200

    few corrections

--- public/index.php
+++ public/index.php
@@ -11,13 +11,14 @@
 <body>
 <div class = "form">
   <h1>Pannel Administration</h1>
-  <form method="post" action="formulaire.php" class="containerstyle">
+  <form method="post" action="traitement.php" class="containerstyle">
   <div class="form-group">
     <label for="nom">Nom de l'animal</label>
     <input class="form-control-sm" type="text" name="nom">
   </div>
+  <div class="form-group">
   <label for="specie">Espèce</label>
-    <select class="form-control" id="specie" name="specie">
+    <select class="form-control-sm" id="specie" name="specie">
       <option>Poissons</option>
       <option>Mollusques</option>
       <option>Cnidaires</option>
@@ -33,6 +34,7 @@
       <option>Cténaires</option>
       <option>Lichens</option>
     </select>
+    </div>
     <div class="form-group">
     <label for="profondeur">Profondeur minimale </label>
     <select class="form-control" id="deep_min" name="deep_min">
@@ -75,14 +77,12 @@
     <input class="form-control-sm" type="text" name="life_time">
   </div>
   <div class="form-group">
-  <label for="weight">Poids moyen</label>
+  <label for="weight">Poids moyen en <strong>Kg</strong></label>
     <input class="form-control-sm" type="text" name="weight">
-    <p>kg</p>
   </div>
   <div class="form-group">
-  <label for="size">taille moyenne</label>
+  <label for="size">taille moyenne en <strong>cm</strong></label>
     <input class="form-control-sm" type="text" name="size">
-    <p>cm</p>
   </div>
   <div class="form-group">
   <label for="life_area">Zone Géographique</label>
@@ -90,7 +90,7 @@
   </div>
   <div class="form-group">
       <label for="description">Description</label>
-      <textarea class="form-control" id="description" rows="3" name="description"></textarea>
+      <textarea class="form-control" id="description" rows="3" cols="45" name="description"></textarea>
     </div>
     <div class="form-group">
       <label for="image_link">Lien image</label>
@@ -112,68 +112,10 @@
             <label for="food">Alt de la vidéo (optionnel)</label>
               <input class="form-control-sm" type="text" name="video_alt">
             </div>
-            <div class="form-group">
-        <label for="espèce">Espèce</label>
-          <input class="form-control-sm" type="text" name="espèce">
-        </div>
-            <input type="submit" value="Valider" />
+            <input class="form-control-sm" type="submit" name="send" value="Ajouter" />
 </form>
 </div>
 
-<?php 
-  $user = "root";  // <- ??? voir fichier includes/connect.php 
-  $pass = "";
-  
-  if(isset($_POST)){
-    $specie = $_POST['specie'];
-    $nom = $_POST['nom'];
-    $deep_min = $_POST['deep_min'];
-    $deep_max = $_POST['deep_max'];
-    $life_time = $_POST['life_time'];
-    $weight = $_POST['weight'];
-    $size = $_POST['size'];
-    $life_area = $_POST['life_area'];
-    $description = $_POST['description'];
-    $image = $_POST['image_link'];
-    $image_alt = $_POST['image_alt'];
-    $reproduction = $_POST['reproduction'];
-    $food = $_POST['food'];
-    $video = $_POST['video_link'];
-    $video_alt = $_POST['video_alt'];
-  }
-
-    try{
-      //instructions à faire dont certaines (ou toutes) menent à une exception
-      $pdo = new PDO("mysql:host=localhost;dbname=api_PAL", $user, $pass);
-      $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
-      $sth = $pdo->prepare(
-      INSERT INTO `species`(`id`, `specie`, `name`, `deep_min`, `deep_max`, `life_time`, `weight`, `size`, `life_area`, `description`, `image_link`, `image_alt`, `reproduction`, `food`, `video_link`, `video_alt`)
-       VALUES (:specie,:nom,:deep_min,:deep_max,:life_time,:weight,:size,:life_area,:description,:image_link,:image_alt,:reproduction,:food,:video_link,:video_alt);
-      $sth->bindParam(":specie",$specie);
-      $sth->bindParam(":nom",$nom);
-      $sth->bindParam(":deep_min",$deep_min);
-      $sth->bindParam(":deep_max",$deep_max);
-      $sth->bindParam(":life_time",$life_time);
-      $sth->bindParam(":weight",$weight);
-      $sth->bindParam(":size",$size);
-      $sth->bindParam(":life_area",$life_area);
-      $sth->bindParam(":description",$description);
-      $sth->bindParam(":image_link",$image);
-      $sth->bindParam(":image_alt",$image_alt);
-      $sth->bindParam(":reproduction",$reproduction);
-      $sth->bindParam(":food",$food);
-      $sth->bindParam("video_link",$video);
-      $sth->bindParam(":video_alt",$video_alt);
-  
-      $sth->execute();
-
-    }
-    catch(PDOException $e){
-      echo 'Impossible de traiter les données. Erreur : '.$e->getMessage();
-    }
-  
-  ?>
 
 
 </body>
